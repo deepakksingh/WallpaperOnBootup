e0e4aa67346fdeb781005285b32b04f6d83c7e59
Deepak Kumar Singh <deepak.singh@research.iiit.ac.in>
2019-06-25 02:24:47 +0530

    use requests module

--- api_base/api_framework.py
+++ api_base/api_framework.py
@@ -3,8 +3,9 @@
 This contains utility functions to make api calls from https://api.unsplash.com/
 
 """
+import requests
 
-#TODO: use urlrequest or such modules to call apis
+#TODO: use requests module to call apis
 
 class APIFramework:
     '''contains all necessary methods to make url requests'''
@@ -13,10 +14,21 @@ class APIFramework:
         '''initializer method'''
         pass
 
-    def make_request(self,url):
-        '''makes single request for the given url'''
+    def make_get_request(self, url):
+        '''makes single GET request for the given url'''
+        resp = requests.get(url)
+        return resp.text
+
+    def make_post_request(self, url, data_dict):
+        '''makes single POST request for the given url'''
         pass
     
     def get_latest_image(self):
         '''should get the latest image on triggered'''
         pass
+
+if __name__ == "__main__":
+    obj_api_framework = APIFramework()
+    res = obj_api_framework.make_get_request('https://www.google.com/')
+    print(res)
+
