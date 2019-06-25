52f7c4d053c78c26b5c75cbe768a1d6406d71f98
Deepak Kumar Singh <deepak.singh@research.iiit.ac.in>
2019-06-25 22:45:18 +0530

    implemented GET request

--- api_base/api_framework.py
+++ api_base/api_framework.py
@@ -1,9 +1,8 @@
 """
-
 This contains utility functions to make api calls from https://api.unsplash.com/
-
 """
 import requests
+from requests.exceptions import HTTPError
 
 #TODO: use requests module to call apis
 
@@ -14,10 +13,24 @@ class APIFramework:
         '''initializer method'''
         pass
 
-    def make_get_request(self, url):
+    def make_get_request(self, url, params=None):
         '''makes single GET request for the given url'''
-        resp = requests.get(url)
-        return resp.text
+        try:
+            response = requests.get(url,params = params)
+            response.raise_for_status()
+
+        except HTTPError as http_err:
+            print(f'HTTP error occured: {http_err}')
+        except Exception as err:
+            print(f'Other error occured: {err}')
+        
+        else:
+            if response.status_code == 200:
+                print("status:" + str(response.status_code))
+                return response
+            else:
+                print("Request Status NOT 200 but: " + str(response.status_code))
+            
 
     def make_post_request(self, url, data_dict):
         '''makes single POST request for the given url'''
@@ -29,6 +42,13 @@ class APIFramework:
 
 if __name__ == "__main__":
     obj_api_framework = APIFramework()
-    res = obj_api_framework.make_get_request('https://www.google.com/')
-    print(res)
+    params = {
+        'q': 'requests+language:python'
+    }
+    response = obj_api_framework.make_get_request('https://api.github.com/search/repositories', params)
+    json_response = response.json()
+    print(json_response)
+    repository = json_response['items'][0]
+    print(f'Repository name: {repository["name"]}')
+    print(f'Repository Description: {repository["description"]}')
 
